dd8eb12a72da8538c7be0fcf8ce6bd903956e9c2
Kayla Bowman - Data Science Master's Candidate <152674435+KaylaBowman@users.noreply.github.com>
2024-10-17 01:29:11 -0400

    Add files via upload

--- real_prac_1.py
+++ real_prac_1.py
@@ -13,7 +13,7 @@ selected_category = st.selectbox("Choose one:", categories)
 st.write(f"You selected: {selected_category}")
 
 #markdown section
-st.markdown("# Load The Data")
+st.subheader("What does the data look like?")
 st.markdown("* found this dataset on [Kaggle](https://www.kaggle.com/datasets/catherinerasgaitis/mxmh-survey-results)")
 
 #load the Data
@@ -23,15 +23,41 @@ mxmh_survey_results = pd.read_csv("mxmh_survey_results.csv")
 st.write(mxmh_survey_results.head())  
 
 #markdown section
-st.markdown("The frequency columns are categorical. We'll have to recode those to be numeric so we can run correlations.")
-st.markdown("What are our features?")
+st.subheader("Data Types")
+st.write(Data Types:
 
-#number of features
-num_features = len(mxmh_survey_results.columns)
-st.write(f"Number of features: {num_features}")
-
-#names of the features
-st.write("Features in the dataset:")
-st.write(mxmh_survey_results.columns.tolist())  
+* Timestamp: Interval 
+* Age: Ratio
+* Primary streaming service: Nominal
+* Hours per day: Ratio
+* While working: Binary (Yes, No)
+* Instrumentalist: Binary (Yes, No)
+* Composer: Binary (Yes, No)
+* Fav genre: Nominal
+* Exploratory: Binary (Yes, No)
+* Foreign languages: Binary (Yes, No)
+* BPM: Ratio 
+* Frequency [Classical]: Ordinal (Never, Rarely, Sometimes, Very Frequently)
+* Frequency [Country]: Ordinal (Never, Rarely, Sometimes, Very Frequently)
+* Frequency [EDM]: Ordinal (Never, Rarely, Sometimes, Very Frequently)
+* Frequency [Folk]: Ordinal (Never, Rarely, Sometimes, Very Frequently)
+* Frequency [Gospel]: Ordinal (Never, Rarely, Sometimes, Very Frequently)
+* Frequency [Hip hop]: Ordinal (Never, Rarely, Sometimes, Very Frequently)
+* Frequency [Jazz]: Ordinal (Never, Rarely, Sometimes, Very Frequently)
+* Frequency [K pop]: Ordinal (Never, Rarely, Sometimes, Very Frequently)
+* Frequency [Latin]: Ordinal (Never, Rarely, Sometimes, Very Frequently)
+* Frequency [Lofi]: Ordinal (Never, Rarely, Sometimes, Very Frequently)
+* Frequency [Metal]: Ordinal (Never, Rarely, Sometimes, Very Frequently)
+* Frequency [Pop]: Ordinal (Never, Rarely, Sometimes, Very Frequently)
+* Frequency [R&B]: Ordinal (Never, Rarely, Sometimes, Very Frequently)
+* Frequency [Rap]: Ordinal (Never, Rarely, Sometimes, Very Frequently)
+* Frequency [Rock]: Ordinal (Never, Rarely, Sometimes, Very Frequently)
+* Frequency [Video game music]: Ordinal (Never, Rarely, Sometimes, Very Frequently)
+* Anxiety: Ordinal (Likert scale, 1-10)
+* Depression: Ordinal (Likert scale, 1-10)
+* Insomnia: Ordinal (Likert scale, 1-10)
+* OCD: Ordinal (Likert scale, 1-10)
+* Music effects: Ordinal (No effect, Improve, Worsen)
+* Permissions: Binary (Yes, No) )  
 
 
