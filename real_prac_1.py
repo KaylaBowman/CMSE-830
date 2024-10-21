47857e0c78da5817b86b06b203bf1b70388eb658
Kayla Bowman - Data Science Master's Candidate <152674435+KaylaBowman@users.noreply.github.com>
2024-10-21 15:04:14 -0400

    Update real_prac_1.py

--- real_prac_1.py
+++ real_prac_1.py
@@ -12,7 +12,7 @@ selected_category = st.selectbox("Choose a section of this project to explore:",
 
 #title of the app
 st.title("Welcome To My Music Therapy App")
-st.markdown("Please be advised that all recommendations are based on self-reported mental health scores of listeners. Since these recommendations are based on the correlations between listening preferences and mental health, they are not proven to cause changes in mood, but rather are associated with changes in mood.")
+st.markdown("Please be advised that all recommendations are based on self-reported mental health scores of listeners. Since these recommendations are based on the correlations between listening preferences and mental health, they are not proven to *cause* changes in mood, but rather are *associated* with changes in mood.")
 
 if selected_category == "Data Overview":
     
