80f22e85f6bad544176db8811337c5a27fa3bcc6
Kayla Bowman - Data Science Master's Candidate <152674435+KaylaBowman@users.noreply.github.com>
2024-12-01 20:35:31 -0500

    Update real_prac_1.py

--- real_prac_1.py
+++ real_prac_1.py
@@ -20,7 +20,7 @@ st.markdown("Please be advised that all recommendations are based on self-report
 
 option = st.sidebar.selectbox(
     "Choose an option:",
-    ["Overview", "Investigate Data", "Clean Data", "Explore Data"]
+    ["Get Recommendations", "Behind The Scenes: Project Steps]
 )
 
 
